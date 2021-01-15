cbc7830c8300c91b5376e57628f43ed0b4d0e75a
JakeDukin <jake.dukin@bohemian.studio>
2021-01-15 14:59:39 +0100

    movie controller crud added

--- CinemaApi/Controllers/MoviesController.cs
+++ CinemaApi/Controllers/MoviesController.cs
@@ -38,20 +38,29 @@ namespace CinemaApi.Controllers
 
         // POST api/<MoviesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Movie movie)
         {
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
         }
 
         // PUT api/<MoviesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Movie movie)
         {
+            var item = _dbContext.Movies.Find(id);
+            item.Name = movie.Name;
+            item.Language = movie.Language;
+            _dbContext.SaveChanges();
         }
 
         // DELETE api/<MoviesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var movie = _dbContext.Movies.Find(id);
+            _dbContext.Movies.Remove(movie);
+            _dbContext.SaveChanges();
         }
     }
 }
